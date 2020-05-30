3e4c29edae10290adf02efc2b51b5fb60a6b047d
Luiz Eduardo de Oliveira Fonseca <luizeof@gmail.com>
2020-05-30 12:27:28 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * file is included before the parent theme's file, so the child theme
  * functions would be used.
  *
- * Text Domain: powertic_oceanwp
+ * Text Domain: luizeof_oceanwp
  * @link http://codex.wordpress.org/Plugin_API
  *
  */
@@ -19,71 +19,49 @@
  /**
  * Add Lead Class to First Paragraph
  */
-function powertic_oceanwp_first_paragraph( $content ) {
+function luizeof_oceanwp_first_paragraph( $content ) {
 	return preg_replace( '/<p([^>]+)?>/', '<p$1 class="lead">', $content, 1 );
 }
-add_filter( 'the_content', 'powertic_oceanwp_first_paragraph' );
+add_filter( 'the_content', 'luizeof_oceanwp_first_paragraph' );
 
 
  /**
  * Add Tags for Attachments
  */
-function powertic_oceanwp_add_tags_for_attachments() {
+function luizeof_oceanwp_add_tags_for_attachments() {
 	register_taxonomy_for_object_type( 'post_tag', 'attachment' );
 }
-add_action( 'init' , 'powertic_oceanwp_add_tags_for_attachments' );
+add_action( 'init' , 'luizeof_oceanwp_add_tags_for_attachments' );
 
 
  /**
  * Add Categories for Attachments
  */
-function powertic_oceanwp_add_categories_for_attachments() {
+function luizeof_oceanwp_add_categories_for_attachments() {
 	register_taxonomy_for_object_type( 'category', 'attachment' );
 }
-add_action( 'init' , 'powertic_oceanwp_add_categories_for_attachments' );
+add_action( 'init' , 'luizeof_oceanwp_add_categories_for_attachments' );
 
 
  /**
-  * Add the OceanWP Settings metabox in your CPT
+  * Add the OceanWP Settings metabox in all your CPT
   */
- function powertic_oceanwp_metabox( $types ) {
+ function luizeof_oceanwp_metabox( $types ) {
   foreach ( get_post_types( '', 'names' ) as $post_type ) {
     $types[] = $post_type;
   }
-	$types[] = "landing_pages";
-	$types[] = "thankyou_pages";
- 	// Return
- 	return $types;
+	return $types;
  }
- add_filter( 'ocean_main_metaboxes_post_types', 'powertic_oceanwp_metabox', 20 );
+ add_filter( 'ocean_main_metaboxes_post_types', 'luizeof_oceanwp_metabox', 20 );
 
 
 /**
  * Load the parent style.css file
- *
- * @link http://codex.wordpress.org/Child_Themes
  */
 function oceanwp_child_enqueue_parent_style() {
-	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
 	$theme   = wp_get_theme( 'OceanWP' );
 	$version = $theme->get( 'Version' );
-	// Load the stylesheet
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
-    // If RTL, uncomment this line
-    /*if ( is_rtl() ) {
-		wp_enqueue_style( 'oceanwp-rtl', get_template_directory_uri() . '/rtl.css', array(), $version );
-	}*/
-
 }
 add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
-
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/luizeof/luizeof-oceanwp-child-theme',
-	__FILE__,
-	'oceanwp-child-theme'
-);
-
-//Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('master');
